836b8982a7875967ad0e98fb63c6391253932ba6
Fraska <58796786+fraska20@users.noreply.github.com>
2023-05-29 12:23:46 +0700

    update

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default axios.create({
   // baseURL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080/api',
-  baseURL: 'https://notes-backend-65wj762j5a-uc.a.run.app/api',
+  baseURL: 'https://backend-notes-65wj762j5a-uc.a.run.app/api',
   headers: {
     "Content-type": "application/json"
   }
